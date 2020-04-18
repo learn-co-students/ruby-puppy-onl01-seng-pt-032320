e4721a88429b839cacb5752f01a9abbc5c8f941f
Malcom Fetus <sora.vista@gmail.com>
2020-04-18 22:42:18 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+require 'pry'
+class Dog
+  attr_accessor :name
+
+@@all = []
+
+def initialize(name)
+  @name = name
+   self.save
+end
+
+def self.clear_all
+  @@all.clear
+end
+
+def self.all
+  @@all
+end
+
+def self.print_all
+#binding.pry
+  @@all.each do |dog|
+     puts dog.name
+  end
+end
+
+def save
+  @save
+  @@all << self
+end
+
+end
+
+dog = Dog.new("Snoopy")
